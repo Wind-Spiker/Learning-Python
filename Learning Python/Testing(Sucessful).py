32619b3c214d399b69bbbdfb4a4f184f2051a259
Wind Spiker <86278792+Wind-Spiker@users.noreply.github.com>
2022-03-24 21:42:45 +0530

    Add files via upload

---
+++ Learning Python/Testing(Sucessful).py
@@ -0,0 +1,26 @@
+print("Hello World")
+Character_Name = "Randy"
+Character_Age = "11"
+print("There was a kid named " + Character_Name + ",")
+print("He was " + Character_Age + " old.")
+print("______")
+print("|    |")
+print("|    |")
+print("|    |")
+print("|____|")
+
+print("Friends Circle")
+print("Friends\nCircle")
+print("Friends\"Circle")
+print("Circle/Sphere")
+
+phrase = "Friends Circle"
+print(phrase + " is a discord server filled with amazing people")
+print(phrase.lower())
+print(phrase.upper())
+print(phrase.isupper())
+print(phrase.lower().islower())
+print(len(phrase))
+print(phrase[6])
+print(phrase.index("c"))
+print(phrase.replace("Friends", "Enemies"))
